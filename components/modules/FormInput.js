6dfc10ec35aca4b8bf5894804748cf9f21e6ca24
Erfanmah7 <96947078+Erfanmah7@users.noreply.github.com>
2025-03-19 17:43:15 +0330

    add form

---
+++ components/modules/FormInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function FormInput({ name,label, type, value,  onChange }) {
+  return (
+    <div className="form-input">
+      <label htmlFor={name}>{label}</label>
+      <input type={type} name={name} value={value} onChange={onChange} />
+    </div>
+  );
+}
+
+export default FormInput;
